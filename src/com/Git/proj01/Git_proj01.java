52a1bb619821f71b87a91bc730e0aa75d57f5a27
Abhijit Samantaray <Abhijit Samantaray@LAPTOP-IS27TH9F>
2019-10-19 23:37:19 +0530

    New project.... Everybody be focused....

---
+++ src/com/Git/proj01/Git_proj01.java
@@ -0,0 +1,37 @@
+package com.Git.proj01;
+
+public class Git_proj01 {
+
+	private String radiant;
+	private String coolant;
+	private String reductant;
+
+	public String getRadiant() {
+		return radiant;
+	}
+
+	public void setRadiant(String radiant) {
+		this.radiant = radiant;
+	}
+
+	public String getCoolant() {
+		return coolant;
+	}
+
+	public void setCoolant(String coolant) {
+		this.coolant = coolant;
+	}
+
+	public String getReductant() {
+		return reductant;
+	}
+
+	public void setReductant(String reductant) {
+		this.reductant = reductant;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
